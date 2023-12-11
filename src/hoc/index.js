febaf3bab1bb78b2c8fb95a44dec15cf67331d46
Safari Germain <99614251+germainsafari@users.noreply.github.com>
2023-12-11 21:04:32 +0100

    Update index.js

--- src/hoc/index.js
+++ src/hoc/index.js
@@ -1,3 +1,18 @@
 import SectionWrapper from "./SectionWrapper";
 
+const SectionWrapper = () => {
+  // Your component logic goes here
+
+  return (
+    // Your JSX for the SectionWrapper component
+    <div>
+      {/* Content of the SectionWrapper component */}
+    </div>
+  );
+};
+
+// Export the SectionWrapper component
+export default SectionWrapper;
+
+
 export { SectionWrapper };
